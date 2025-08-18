3e67bd4e658ba431054f4f64484ab10c252de040
Jonathan <jonfritz@gmail.com>
2025-08-17 21:18:59 -0400

    Ensured that files from the src/main/resources directory in the engine module are available to the bouncing-balls module at runtime

--- bouncing-balls/build.gradle.kts
+++ bouncing-balls/build.gradle.kts
@@ -24,3 +24,12 @@ kotlin {
         languageVersion.set(JavaLanguageVersion.of(21))
     }
 }
+
+// copies src/main/resources from the engine project to the bouncing-balls project so that those resources
+// exist in the classpath when running the bouncing-balls project.
+// this may not be necessary if/when the engine dependency is compiled into a fat jar for distribution.
+tasks.processResources {
+    from(project(":engine").projectDir.resolve("src/main/resources")) {
+        into(".")
+    }
+}
